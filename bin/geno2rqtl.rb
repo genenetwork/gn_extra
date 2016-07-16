30b195f918334e82c2ba685107ff912bbb02fdf7
Pjotr Prins <pjotr.public01@thebird.nl>
2016-07-16 12:52:07 +0600

    geno2rqtl: write cross_geno.csv

--- bin/geno2rqtl.rb
+++ bin/geno2rqtl.rb
@@ -12,18 +12,34 @@ inds = cols = nil
 
 ARGV.each do |fn|
   print "Parsing #{fn}\n"
+  base = File.basename(fn,".geno")
+  geno = base + '_geno.csv'
+  gmap = base + '_gmap.csv'
+  geno_f = File.open(geno,"w")
+  gmap_f = File.open(gmap,"w")
+
+  print "Writing #{geno}, #{gmap}...\n"
+  count = 0
   File.open(fn).each_line do |l|
     l =~ /^@(\S+):(\S+)/
     if $1
+      # ---- Header meta-info
       h[$1] = $2
     elsif l =~ /Chr\tLocus/i
+      # ---- Column info
       cols = l.chomp.split(/\t/)
       # p cols
       inds = cols[4..-1]
       # p inds
+      geno_f.write((["id"]+inds).join(",")+"\n")
     else
-      fields = l.chomp.split(/t/)
+      # ---- Genotypes
+      count += 1
+      fields = l.chomp.split(/\t/)
       p fields
+      geno_f.write(count)
+      geno_f.write(","+fields[4..-1].join(","))
+      geno_f.write("\n")
     end
   end
 end
